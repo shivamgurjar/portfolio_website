6f8350e302436abea8d080c03da46d99b1988f4f
Shivam <shivamgurjar1008@gmail.com>
2021-05-23 16:36:42 +0530

    Added resume utility

---
+++ app.js
@@ -0,0 +1,16 @@
+
+const open_resume = document.getElementById("resume_button");
+
+open_resume.addEventListener('click', (e)=>{
+    window.location.href = "resume.html";
+})
+
+// if(screen.width <= 799){
+//     open_resume.addEventListener('click', (e)=>{
+//         window.location.href = "resume_mobile.html";
+//     })
+// }else{
+//     open_resume.addEventListener('click', (e)=>{
+//         window.location.href = "resume.html";
+//     })
+// }
---
+++ resume.js
@@ -0,0 +1,7 @@
+
+const back = document.getElementById("home_button");
+
+back.addEventListener('click', (e)=>{
+    window.location.href = "index.html";
+})
+
